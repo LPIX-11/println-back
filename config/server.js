1d7f2f14155cc91b323a2435680d906701207e64
Johnson Mohamed <mohamedjohnson31@gmail.com>
2022-03-28 08:54:59 +0000

    :wrench: Added Saved Watchtime

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b397960f9c165b01ae2147a12ac7d57b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f3a69af7-dc99-463b-8f73-d40cc6903a0f'
+};
